94cdd91bf63cf8fb341ca11daffe2d4b8e6bbf12
duanf <1234QWER!>
2020-11-13 15:16:16 +0800

    试试

---
+++ JenkinsGroovy/JR_test.groovy
@@ -0,0 +1,43 @@
+node {
+
+    def branch = scm.branches[0].name
+
+    stage('checkout') {
+
+        def paramBranch = "${env.BRANCH}"
+        def nullStr = "null"
+        if(paramBranch?.trim()&&(nullStr!=paramBranch)){
+            echo paramBranch
+            branch = "${env.BRANCH}"
+            echo "set empty"
+        }
+        echo "代码分支"+branch
+        checkout([
+                $class: 'GitSCM',
+                branches: [[name: branch]],
+                doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
+                extensions: scm.extensions,
+                userRemoteConfigs: scm.userRemoteConfigs
+        ])
+
+    }
+
+    stage('test') {
+
+        def maven = docker.image('maven:3-alpine')
+        maven.inside('-v /root/.m2/:/root/.m2/'){
+            sh 'mvn clean package -f pom.xml -Dmaven.test.skip=true'
+//            sh 'mvn clean -f xuanzhuang-api-test/pom.xml test '
+        }
+        sh 'java -jar target/testng-1.0-SNAPSHOT.jar dev'
+    }
+
+
+    stage('AfterTest') {
+    }
+
+
+
+
+
+}
